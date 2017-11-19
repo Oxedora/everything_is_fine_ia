416d6dd244653b1e60d9c0dc0f6d9d4e0574514b
Oxedora <ed.breuille@gmail.com>
2017-11-19 12:37:53 +0100

    initial commit

---
+++ Scripts/AgentsScripts/Agent.cs
@@ -0,0 +1,70 @@
+﻿    using UnityEngine;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using UnityEditor;
+
+// Reference :
+// https://processing.org/examples/flocking.html
+
+public class Agent : MonoBehaviour {
+
+    private Rigidbody rb;
+    public Rigidbody Rb {
+    	get {
+    		return rb;
+    	}
+    }
+
+    private AgentsSettings settings;
+    public AgentsSettings Settings {
+    	get {
+    		return settings;
+    	}
+    }
+
+    private Flock flocking;
+    public Flock Flocking {
+    	get {
+    		return flocking;
+    	}
+    }
+
+    private FieldOfView fov;
+    public FieldOfView Fov {
+    	get {
+    		return fov;
+    	}
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		settings = GetComponent<AgentsSettings>();
+		flocking = GetComponent<Flock>();
+		fov = GetComponent<FieldOfView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        List<Agent> neighbors = fov.FindVisibleTargets();
+
+        Vector3 viewAngleA = fov.DirFromAngle(- settings.ViewAngle / 2, false);
+        Vector3 viewAngleB = fov.DirFromAngle(settings.ViewAngle / 2, false);
+
+        Debug.DrawLine(transform.position, transform.position + viewAngleA * settings.ViewRadius);
+        Debug.DrawLine(transform.position, transform.position + viewAngleB * settings.ViewRadius);
+
+        foreach(Agent a in neighbors){
+            Debug.DrawLine(transform.position, a.transform.position, Color.yellow);
+        }
+
+        Vector3 force = flocking.Flocking(neighbors);
+        Vector3 destination = transform.position + force;
+        Debug.DrawLine(transform.position, destination, Color.black);
+        rb.velocity = (force.Equals(Vector3.zero) ? (Vector3)transform.TransformDirection(Vector3.forward) : force) * settings.MaxSpeed;
+        transform.rotation = Quaternion.LookRotation(rb.velocity);
+    }
+
+}
---
+++ Scripts/BehaviorScript/FieldOfView.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour {
+
+    private Agent myAgent;
+
+    public Agent MyAgent
+    {
+        get
+        {
+            return myAgent;
+        }
+    }
+
+    private void Start()
+    {
+        myAgent = GetComponent<Agent>();
+    }
+
+    public List<Agent> FindVisibleTargets()
+    {
+        List<Agent> visibleTargets = new List<Agent>();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, MyAgent.Settings.ViewRadius, MyAgent.Settings.TargetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Agent target = targetsInViewRadius[i].gameObject.GetComponent<Agent>();
+            if(target != null){
+                Vector3 dirToTarget = (target.transform.position - transform.position).normalized;
+
+                if (Vector3.Angle(transform.forward, dirToTarget) < MyAgent.Settings.ViewAngle / 2)
+                {
+                    float dstToTarget = Vector3.Distance(transform.position, target.transform.position);
+
+                    if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, MyAgent.Settings.ObstacleMask))
+                    {
+                        visibleTargets.Add(target);
+                    }
+                }
+            }
+        }
+
+        return visibleTargets;
+    }
+
+    public List<Agent> FindCloseTargets()
+    {
+        List<Agent> visibleTargets = new List<Agent>();
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, MyAgent.Settings.SafeSpace, MyAgent.Settings.TargetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Agent target = targetsInViewRadius[i].gameObject.GetComponent<Agent>();
+            if(target != null){
+                Vector3 dirToTarget = (target.transform.position - transform.position).normalized;
+                float dstToTarget = Vector3.Distance(transform.position, target.transform.position);
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, MyAgent.Settings.ObstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+
+        return visibleTargets;
+    }
+
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal = false)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
