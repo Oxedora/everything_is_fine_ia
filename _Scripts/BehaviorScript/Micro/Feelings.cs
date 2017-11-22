c574a725a48b728ab7bf72609db1c6446ee902dc
celia.rouquairol@etu.umontpellier.fr <celia.rouquairol@etu.umontpellier.fr>
2017-11-22 17:56:04 +0100

    adding feels

--- _Scripts/BehaviorScript/Micro/Feelings.cs
+++ _Scripts/BehaviorScript/Micro/Feelings.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Feelings : MonoBehaviour {
+	[Range(0,1)]
+	private float anger;
+	public float Anger {
+		get {
+			return anger;
+		}
+	}
 
+	[Range(0,1)]
+	private float fear;
+	public float Fear {
+		get {
+			return fear;
+		}
+	}
 	// Use this for initialization
 	void Start () {
-		
+		System.Random rand = new System.Random();
+		anger = (float) rand.Next(0, 10) / (float)10.0;
+		fear = (float) rand.Next(0, 10) / (float)10.0;
 	}
 	
 	// Update is called once per frame
